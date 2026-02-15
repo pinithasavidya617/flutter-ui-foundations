ca29b34cb5edd1b109466bf59ad37d1cf09ff09d
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-02-15 09:19:16 +0530

    Add image screen

---
+++ lib/screens/image_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ImageScreen extends StatefulWidget {
+  const ImageScreen({super.key});
+
+  @override
+  State<ImageScreen> createState() => _ImageScreenState();
+}
+
+class _ImageScreenState extends State<ImageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/img/wallet.png'),
+      ),
+    );
+  }
+}
